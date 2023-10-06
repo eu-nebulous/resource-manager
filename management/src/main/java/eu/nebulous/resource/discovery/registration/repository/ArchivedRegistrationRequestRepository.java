dd6312d8f383d65f79efe60663abaae95e3df364
ipatini <ipatini@mail.ntua.gr>
2023-10-06 11:53:07 +0300

    RD: Fixed registration request repositories

--- management/src/main/java/eu/nebulous/resource/discovery/registration/repository/ArchivedRegistrationRequestRepository.java
+++ management/src/main/java/eu/nebulous/resource/discovery/registration/repository/ArchivedRegistrationRequestRepository.java
@@ -1,9 +1,10 @@
 package eu.nebulous.resource.discovery.registration.repository;
 
 import eu.nebulous.resource.discovery.registration.model.ArchivedRegistrationRequest;
-import eu.nebulous.resource.discovery.registration.model.RegistrationRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ArchivedRegistrationRequestRepository extends MongoRepository<ArchivedRegistrationRequest, String> {
-	RegistrationRequest findByRequester(String requester);
+	List<ArchivedRegistrationRequest> findByRequester(String requester);
 }
--- management/src/main/java/eu/nebulous/resource/discovery/registration/repository/RegistrationRequestRepository.java
+++ management/src/main/java/eu/nebulous/resource/discovery/registration/repository/RegistrationRequestRepository.java
@@ -3,6 +3,8 @@ package eu.nebulous.resource.discovery.registration.repository;
 import eu.nebulous.resource.discovery.registration.model.RegistrationRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface RegistrationRequestRepository extends MongoRepository<RegistrationRequest, String> {
-	RegistrationRequest findByRequester(String requester);
+	List<RegistrationRequest> findByRequester(String requester);
 }
