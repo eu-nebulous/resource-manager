ccd667ae147ebb5081a5137f6d521f266a96dbda
ipatini <ipatini@mail.ntua.gr>
2023-10-17 10:52:51 +0300

    RD: Changed authentication to use BCrypt-encrypted passwords

--- management/src/main/java/eu/nebulous/resource/discovery/SecurityConfig.java
+++ management/src/main/java/eu/nebulous/resource/discovery/SecurityConfig.java
@@ -4,18 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.security.SecureRandom;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -39,21 +39,31 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
-        properties.getUsers().forEach(userData -> {
-            UserDetails user = User.withUsername(userData.getUsername())
-                    .password(encoder().encode(userData.getPassword()))
-                    .roles(userData.getRoles().toArray(new String[0]))
-                    .build();
-            users.add(user);
-        });
-        return new InMemoryUserDetailsManager(users.toArray(new UserDetails[0]));
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder());
     }
 
     @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                properties.getUsers().stream()
+                        .map(userData -> User.builder()
+                                .username(userData.getUsername())
+                                .password(userData.getPassword())
+                                .roles(userData.getRoles().toArray(new String[0]))
+                                .build())
+                        .toList());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        int strength = 10;  // iterations
+        return new BCryptPasswordEncoder(strength, new SecureRandom());
+    }
+
+    /*@Bean
     public PasswordEncoder encoder() {
+        // Clear-text password encoder
         return new PasswordEncoder() {
             @Override
             public String encode(CharSequence rawPassword) {
@@ -65,5 +75,5 @@ public class SecurityConfig {
                 return rawPassword.toString().equals(encodedPassword);
             }
         };
-    }
+    }*/
 }
