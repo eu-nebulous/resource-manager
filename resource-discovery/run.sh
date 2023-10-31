2cf4d6a7f9459a396c6955b7168dbd26d2bbd601
ipatini <ipatini@mail.ntua.gr>
2023-10-31 11:22:19 +0200

    Use the right container image
    * Updated Zuul and Helm files with the right settings
    * Cleared Buildpacks settings, and added Dockerfile
    
    Change-Id: I35fc05e34fad405e0981f822e39139d7d244252e

---
+++ resource-discovery/run.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Change directory to RD home
+PREVWORKDIR=`pwd`
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${BASEDIR}
+
+# Read JASYPT password (decrypts encrypted configuration settings)
+#if [[ -z "$JASYPT_PASSWORD" ]]; then
+#    printf "Configuration Password: "
+#    read -s JASYPT_PASSWORD
+#    export JASYPT_PASSWORD
+#fi
+# Use this online service to encrypt/decrypt passwords:
+# https://www.devglan.com/online-tools/jasypt-online-encryption-decryption
+
+
+# Setup TERM & INT signal handler
+trap 'echo "Signaling server to exit"; kill -TERM "${pid}"; wait "${pid}"; ' SIGTERM SIGINT
+
+# Set JRE command and options
+JRE=/opt/java/openjdk/bin/java
+#JAVA_ADD_OPENS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util.regex=ALL-UNNAMED --add-opens java.base/sun.nio.cs=ALL-UNNAMED --add-opens java.base/java.nio.charset=ALL-UNNAMED"
+
+# Set shell encoding to UTF-8 (in order to display banner correctly)
+export LANG=C.UTF-8
+
+# Print basic env. info
+echo "--------------------------------------------------------------------------------"
+echo "Env. info:"
+echo "LANG: ${LANG}"
+echo "USER: $( whoami )"
+echo "IP address: `hostname -I`"
+echo "--------------------------------------------------------------------------------"
+echo "JRE:"
+${JRE} -version
+echo "--------------------------------------------------------------------------------"
+echo "Starting Resource Discovery server..."
+
+# Run RD server
+${JRE} \
+    $JAVA_OPTS \
+    $JAVA_ADD_OPENS \
+    -Djasypt.encryptor.password=$JASYPT_PASSWORD \
+    -Djava.security.egd=file:/dev/urandom \
+    org.springframework.boot.loader.JarLauncher \
+    $* &
+
+# Get PID and wait it to exit
+pid=$!
+echo "Pid: $pid"
+wait $pid
+echo "Server exited"
+
+cd $PREVWORKDIR
