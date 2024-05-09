68b8bc9f34dfffd123eae745bfc919bb6edb8cd2
ipatini <ipatini@mail.ntua.gr>
2024-05-09 13:10:58 +0300

    RD: Updated wait_for_mongodb.sh script and commented out liveness and readiness checks
    Change-Id: I989b153f1c3df8016b838488672b30e062ef2656

--- resource-discovery/wait_for_mongodb.sh
+++ resource-discovery/wait_for_mongodb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define MongoDB connection details
-MONGODB_HOST="localhost"
-MONGODB_PORT="27017"
+if [ -z "$MONGODB_HOST" ]; then MONGODB_HOST="localhost"; fi
+if [ -z "$MONGODB_PORT" ]; then MONGODB_PORT="27017"; fi
 
 # Function to check if MongoDB is ready
 wait_for_mongodb() {
-    echo "Waiting for MongoDB to be ready..."
+    echo "Waiting for MongoDB ($MONGODB_HOST:$MONGODB_PORT) to be ready..."
     until nc -z $MONGODB_HOST $MONGODB_PORT
     do
         sleep 1
