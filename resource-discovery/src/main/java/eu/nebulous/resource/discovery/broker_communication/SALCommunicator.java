9aa91c14f088f0a668f76933310d993e234bcadc
ipatini <3739531+ipatini@users.noreply.github.com>
2024-10-15 12:53:19 +0300

    Use device reference instead of name to register to SAL (#10)

--- resource-discovery/src/main/java/eu/nebulous/resource/discovery/broker_communication/SALCommunicator.java
+++ resource-discovery/src/main/java/eu/nebulous/resource/discovery/broker_communication/SALCommunicator.java
@@ -142,7 +142,7 @@ public class SALCommunicator {
     }
 
 
-    public static String get_device_registration_json(String internal_ip_address, String external_ip_address, int external_access_port, String os_family, String os_architecture, String jar_url, int os_version, int cpu_cores, int ram_gb, int disk_gb, String device_name,String provider_id, String city_name, String country_name, String device_username, String device_password, String private_key, double device_longitude, double device_latitude) {
+    public static String get_device_registration_json(String internal_ip_address, String external_ip_address, int external_access_port, String os_family, String os_architecture, String jar_url, int os_version, int cpu_cores, long ram_gb, long disk_gb, String device_name,String provider_id, String city_name, String country_name, String device_username, String device_password, String private_key, double device_longitude, double device_latitude) {
 
             JSONObject root_json_object = new JSONObject();
             JSONObject loginCredential = new JSONObject();
