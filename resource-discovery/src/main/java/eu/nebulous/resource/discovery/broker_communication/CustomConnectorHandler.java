3abb2c2db4aca3b8c9e29a3d976e18e4d2d98f7a
atsag <andreastsagkaropoulos@hotmail.com>
2024-11-12 14:40:56 +0200

    Only try to stop the connector if it has been previously initialized

--- resource-discovery/src/main/java/eu/nebulous/resource/discovery/broker_communication/CustomConnectorHandler.java
+++ resource-discovery/src/main/java/eu/nebulous/resource/discovery/broker_communication/CustomConnectorHandler.java
@@ -3,12 +3,19 @@ package eu.nebulous.resource.discovery.broker_communication;
 import eu.nebulouscloud.exn.core.Context;
 import eu.nebulouscloud.exn.handlers.ConnectorHandler;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class CustomConnectorHandler extends ConnectorHandler {
     private Context context;
+    private final AtomicBoolean ready = new AtomicBoolean(false);
 
     @Override
     public void onReady(Context context) {
         this.context = context;
+        synchronized (ready) {
+            this.ready.set(true);
+            this.ready.notify();
+        }
     }
     public void remove_consumer_with_key(String key){
         context.unregisterConsumer(key);
@@ -20,5 +27,9 @@ public class CustomConnectorHandler extends ConnectorHandler {
 
     public void setContext(Context context) {
         this.context = context;
+    }
+
+    public AtomicBoolean getReady() {
+        return ready;
     }
 }
